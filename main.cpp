0b06487b3b20cdffbae4355c9deea2159efb58bb
YuriiAndrukhiv <yuri.andrukhiv@gmail.com>
2019-06-13 15:28:45 +0300

    Created program where window is close after clicked on the button

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include "widget.h"
+
+
+int main (int lArgc, char *lArgv[])
+{
+    QApplication lApplication (lArgc, lArgv);
+
+    Widget lWidget;
+    lWidget.show();
+
+
+    return lApplication.exec();
+
+}
